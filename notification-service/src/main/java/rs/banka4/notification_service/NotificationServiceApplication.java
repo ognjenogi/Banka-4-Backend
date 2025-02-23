fa8b094e8bbb65069cee220915b9f4a87546ae8e
Mehmedalija Karisik <rg.mehmedalija@gmail.com>
2025-02-23 01:00:49 +0100

    Init notification service

---
+++ notification-service/src/main/java/rs/banka4/notification_service/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package rs.banka4.notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
