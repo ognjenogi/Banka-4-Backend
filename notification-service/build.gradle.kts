5d8a5e53bb8a1a3f6e616b296afea49ea6779dae
ognjend05 <ooggii05@gmail.com>
2025-02-25 01:38:27 +0100

    Init and tested RabbitMQ in local

--- notification-service/build.gradle.kts
+++ notification-service/build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-database-postgresql")
+	implementation("org.springframework.boot:spring-boot-starter-amqp")
+	implementation("org.springframework.boot:spring-boot-starter-mail:3.3.0")
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 	compileOnly("org.projectlombok:lombok")
---
+++ notification-service/src/main/java/rs/banka4/notification_service/config/RabbitMQConfig.java
@@ -0,0 +1,7 @@
+package rs.banka4.notification_service.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+}
