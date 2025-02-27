95524e0d1c9d8392ae79a187bf0eb04da077a7ec
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-02-27 22:44:47 +0100

    user-service/OAI: define authentication schemas

--- user-service/src/main/java/rs/banka4/user_service/config/OpenAPIConfig.java
+++ user-service/src/main/java/rs/banka4/user_service/config/OpenAPIConfig.java
@@ -1,10 +1,19 @@
 package rs.banka4.user_service.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@SecuritySchemes({
+        @SecurityScheme(name = "bearerAuth",
+                        type = SecuritySchemeType.HTTP,
+                        scheme = "bearer",
+                        bearerFormat = "JWT access token")
+})
 @Configuration
 public class OpenAPIConfig {
     @Bean
