3f2fc63805bd72c0196f44fa1d8b8fb7b7cea9ec
Mehmedalija Karisik <rg.mehmedalija@gmail.com>
2025-02-27 15:04:56 +0100

    user/verification: modify verification dto

--- user-service/src/main/java/rs/banka4/user_service/dto/requests/EmployeeVerificationRequestDto.java
+++ user-service/src/main/java/rs/banka4/user_service/dto/requests/EmployeeVerificationRequestDto.java
@@ -1,10 +1,13 @@
 package rs.banka4.user_service.dto.requests;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record EmployeeVerificationRequestDto(
-    @JsonProperty("password")
-    String password,
-    @JsonProperty("verification_code")
-    String verificationCode
+        @NotNull(message = "Password cannot be null")
+        @Size(min = 8, max = 32, message = "Password must be between 8 and 32 characters")
+        String password,
+        @NotNull(message = "Verification code cannot be null")
+        String verificationCode
 ) {}
