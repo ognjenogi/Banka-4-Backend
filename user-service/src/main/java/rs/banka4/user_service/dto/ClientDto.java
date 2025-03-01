2ef802890f3a1af05c7db72cfefdea4db06f3fa6
danilojoncic <djoncic14122rn@raf.rs>
2025-03-01 15:32:14 +0100

    implement client dto

---
+++ user-service/src/main/java/rs/banka4/user_service/dto/ClientDto.java
@@ -0,0 +1,41 @@
+package rs.banka4.user_service.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import rs.banka4.user_service.models.Privilege;
+
+import java.time.LocalDate;
+import java.util.EnumSet;
+import java.util.Set;
+
+public record ClientDto(
+        @Schema(description = "Client ID", example = "1fad2c01-f82f-41a6-822c-8ca1b3232575")
+        String id,
+        @Schema(description = "First name", example = "Mehmedalija")
+        String firstName,
+        @Schema(description = "Last name", example = "Doe")
+        String lastName,
+        @Schema(description = "Date of birth", example = "1990-01-01")
+        LocalDate dateOfBirth,
+        @Schema(description = "Gender", example = "Male")
+        String gender,
+        @Schema(description = "Email address", example = "danny.jo@example.com")
+        String email,
+        @Schema(description = "Phone number", example = "123-456-7890")
+        String phone,
+        @Schema(description = "Address", example = "123 Main St")
+        String address,
+        @Schema(description = "Username", example = "dannny")
+        String username,
+        @Schema(description = "Job position", example = "Developer")
+        String position,
+        @Schema(description = "Department", example = "IT")
+        String department,
+        @Schema(description = "List of privileges", example = "[\"ADMIN\"]")
+        EnumSet<Privilege> privileges,
+        @Schema(description = "Indicates if the client is active", example = "false")
+        boolean active,
+        @Schema(description = "Set of accounts linked to one client",example = "[acc1,acc2,acc3]")
+        Set<String> linkedAccounts
+
+) {
+}
