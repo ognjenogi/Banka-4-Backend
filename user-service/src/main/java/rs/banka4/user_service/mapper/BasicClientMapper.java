f20f40a0a6e9cc13f9c99f4f05b8b3eee06d83a5
danilojoncic <djoncic14122rn@raf.rs>
2025-03-01 15:09:38 +0100

    implement service method with mapper skeleton

---
+++ user-service/src/main/java/rs/banka4/user_service/mapper/BasicClientMapper.java
@@ -0,0 +1,13 @@
+package rs.banka4.user_service.mapper;
+
+import org.springframework.stereotype.Component;
+import rs.banka4.user_service.dto.ClientResponseDto;
+import rs.banka4.user_service.models.Client;
+
+@Component
+public class BasicClientMapper {
+
+    public ClientResponseDto entityToDto(Client client) {
+        return null;
+    }
+}
--- user-service/src/main/java/rs/banka4/user_service/service/impl/ClientServiceImpl.java
+++ user-service/src/main/java/rs/banka4/user_service/service/impl/ClientServiceImpl.java
@@ -4,20 +4,34 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import rs.banka4.user_service.dto.ClientResponseDto;
-import rs.banka4.user_service.dto.EmployeeResponseDto;
 import rs.banka4.user_service.exceptions.NotAuthenticated;
-import rs.banka4.user_service.models.Employee;
+import rs.banka4.user_service.exceptions.NotFound;
+import rs.banka4.user_service.mapper.BasicClientMapper;
+import rs.banka4.user_service.models.Client;
 import rs.banka4.user_service.repositories.ClientRepository;
 import rs.banka4.user_service.service.abstraction.ClientService;
+import rs.banka4.user_service.utils.JwtUtil;
 
 @Service
 @RequiredArgsConstructor
 public class ClientServiceImpl implements ClientService {
     private final ClientRepository clientRepository;
+    private final BasicClientMapper basicClientMapper;
+    private final JwtUtil jwtUtil;
 
 
     @Override
     public ResponseEntity<ClientResponseDto> getMe(String authorization) {
-        return null;
+        String token = authorization.replace("Bearer ", "");
+        String clinetUsername = jwtUtil.extractUsername(token);
+
+        if(jwtUtil.isTokenExpired(token)) throw new NotAuthenticated();
+        if(jwtUtil.isTokenInvalidated(token)) throw new NotAuthenticated();
+
+        Client client = clientRepository.findByEmail(clinetUsername).orElseThrow(NotFound::new);
+
+        ClientResponseDto response = basicClientMapper.entityToDto(client);
+        return ResponseEntity.ok(response);
+
     }
 }
