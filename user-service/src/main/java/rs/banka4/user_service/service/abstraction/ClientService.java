cb05ac08cf2fb2c3285c1edac15fcab5ae8c2742
Luka Korica <lukakorica@gmail.com>
2025-03-02 01:35:20 +0100

    Added mock Dto, controller and service

--- user-service/src/main/java/rs/banka4/user_service/service/abstraction/ClientService.java
+++ user-service/src/main/java/rs/banka4/user_service/service/abstraction/ClientService.java
@@ -1,8 +1,18 @@
 package rs.banka4.user_service.service.abstraction;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import rs.banka4.user_service.dto.ClientDto;
+import rs.banka4.user_service.dto.CreateClientDto;
+import rs.banka4.user_service.dto.PrivilegesDto;
+import rs.banka4.user_service.dto.UpdateClientDto;
 
 public interface ClientService {
-    ResponseEntity<ClientDto> getMe(String authorization);
+    ResponseEntity<PrivilegesDto> getPrivileges(String token);
+    ResponseEntity<ClientDto> getMe(String token);
+    ResponseEntity<ClientDto> getClient(String id);
+    ResponseEntity<Void> createClient(CreateClientDto createClientDto);
+    ResponseEntity<Page<ClientDto>> getClients(String firstName, String lastName, String email, String phone, Pageable pageable);
+    ResponseEntity<Void> updateClient(String id, UpdateClientDto updateClientDto);
 }
--- user-service/src/main/java/rs/banka4/user_service/service/impl/ClientServiceImpl.java
+++ user-service/src/main/java/rs/banka4/user_service/service/impl/ClientServiceImpl.java
@@ -1,16 +1,27 @@
 package rs.banka4.user_service.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import rs.banka4.user_service.dto.ClientDto;
+import rs.banka4.user_service.dto.*;
 import rs.banka4.user_service.exceptions.NotAuthenticated;
 import rs.banka4.user_service.exceptions.NotFound;
 import rs.banka4.user_service.mapper.BasicClientMapper;
 import rs.banka4.user_service.models.Client;
+import rs.banka4.user_service.models.Privilege;
 import rs.banka4.user_service.repositories.ClientRepository;
 import rs.banka4.user_service.service.abstraction.ClientService;
 import rs.banka4.user_service.utils.JwtUtil;
+
+import java.time.LocalDate;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +30,11 @@ public class ClientServiceImpl implements ClientService {
     private final BasicClientMapper basicClientMapper;
     private final JwtUtil jwtUtil;
 
+
+    @Override
+    public ResponseEntity<PrivilegesDto> getPrivileges(String token) {
+        return null;
+    }
 
     @Override
     public ResponseEntity<ClientDto> getMe(String authorization) {
@@ -33,5 +49,51 @@ public class ClientServiceImpl implements ClientService {
         ClientDto response = basicClientMapper.entityToDto(client);
         return ResponseEntity.ok(response);
 
+    }
+
+    @Override
+    public ResponseEntity<ClientDto> getClient(String id) {
+        ClientDto clientDto = new ClientDto(
+                id,
+                "MockFirstName",
+                "MockLastName",
+                LocalDate.of(1985, 5, 20),
+                "Male",
+                "mock.email@example.com",
+                "987-654-3210",
+                "123 Mockingbird Lane",
+                EnumSet.noneOf(Privilege.class),
+                List.of()
+        );
+        return ResponseEntity.ok(clientDto);
+    }
+
+    @Override
+    public ResponseEntity<Void> createClient(CreateClientDto createClientDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+
+    @Override
+    public ResponseEntity<Page<ClientDto>> getClients(String firstName, String lastName, String email, String phone, Pageable pageable) {
+        ClientDto clientDto = new ClientDto(
+                UUID.randomUUID().toString(),
+                "MockedFirstName",
+                "MockedLastName",
+                LocalDate.of(1980, 3, 15),
+                "Female",
+                "mocked@example.com",
+                "123-123-1234",
+                "456 Mock Avenue",
+                EnumSet.noneOf(Privilege.class),
+                List.of()
+        );
+        Page<ClientDto> page = new PageImpl<>(List.of(clientDto), pageable, 1);
+        return ResponseEntity.ok(page);
+    }
+
+    @Override
+    public ResponseEntity<Void> updateClient(String id, UpdateClientDto updateClientDto) {
+        return null;
     }
 }
