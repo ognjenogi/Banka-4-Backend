cbd9f902c4ae11c7630807c030ddc001afa16fe7
Vasilije Jukic <vasilijejukic@gmail.com>
2025-03-09 03:13:10 +0100

    VerificationCode tests fix

---
+++ user-service/src/test/java/rs/banka4/user_service/generator/VerificationCodeObjectMother.java
@@ -0,0 +1,35 @@
+package rs.banka4.user_service.generator;
+
+import rs.banka4.user_service.domain.auth.db.VerificationCode;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class VerificationCodeObjectMother {
+
+    public static VerificationCode createValidVerificationCode(String email) {
+        return VerificationCode.builder()
+                .code(UUID.randomUUID().toString())
+                .expirationDate(LocalDateTime.now().plusDays(1))
+                .used(false)
+                .email(email)
+                .build();
+    }
+
+    public static VerificationCode createExpiredVerificationCode(String email) {
+        return VerificationCode.builder()
+                .code(UUID.randomUUID().toString())
+                .expirationDate(LocalDateTime.now().minusDays(1))
+                .used(false)
+                .email(email)
+                .build();
+    }
+
+    public static VerificationCode createUsedVerificationCode(String email) {
+        return VerificationCode.builder()
+                .code(UUID.randomUUID().toString())
+                .expirationDate(LocalDateTime.now().plusDays(1))
+                .used(true)
+                .email(email)
+                .build();
+    }
+}
