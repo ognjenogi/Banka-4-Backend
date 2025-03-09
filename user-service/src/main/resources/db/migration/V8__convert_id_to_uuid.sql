5402d9a8c1467bc0f7bc9701969bca54c9c5fbfb
Vasilije Jukic <vasilijejukic@gmail.com>
2025-03-09 16:21:39 +0100

    refactor finish

--- user-service/src/main/resources/db/migration/V8__convert_id_to_uuid.sql
+++ user-service/src/main/resources/db/migration/V8__convert_id_to_uuid.sql
@@ -1,7 +1,11 @@
+UPDATE employees SET gender = 'MALE' WHERE gender = 'Male';
+UPDATE employees SET gender = 'FEMALE' WHERE gender = 'Female';
+UPDATE clients SET gender = 'MALE' WHERE gender = 'Male';
+UPDATE clients SET gender = 'FEMALE' WHERE gender = 'Female';
+
 -- Drop existing foreign keys
 ALTER TABLE accounts DROP CONSTRAINT fk_accounts_on_client;
 ALTER TABLE accounts DROP CONSTRAINT fk_accounts_on_employee;
-ALTER TABLE client_contacts DROP CONSTRAINT fk_clicon_on_client;
 ALTER TABLE companies DROP CONSTRAINT fk_companies_on_majorityowner;
 
 -- Convert character varying ID columns to UUID
@@ -11,7 +15,7 @@ ALTER TABLE client_contacts ALTER COLUMN client_id TYPE UUID USING client_id::uu
 ALTER TABLE accounts ALTER COLUMN employee_id TYPE UUID USING employee_id::uuid;
 ALTER TABLE accounts ALTER COLUMN client_id TYPE UUID USING client_id::uuid;
 ALTER TABLE companies ALTER COLUMN majority_owner_id TYPE UUID USING majority_owner_id::uuid;
-ALTER TABLE employees ALTER COLUMN id TYPE UUID USING id::uuid;  -- Dodajemo konverziju employees.id
+ALTER TABLE employees ALTER COLUMN id TYPE UUID USING id::uuid;
 
 -- Re-add foreign keys with correct types
 ALTER TABLE accounts ADD CONSTRAINT fk_accounts_on_client FOREIGN KEY (client_id) REFERENCES clients(id);
