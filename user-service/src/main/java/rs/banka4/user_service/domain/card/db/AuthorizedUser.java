f323df6b7ba93fcf507d2b8e97eeac3681d18c7e
ognjend05 <ooggii05@gmail.com>
2025-03-09 22:19:49 +0100

    Written migration file

--- user-service/src/main/java/rs/banka4/user_service/domain/card/db/AuthorizedUser.java
+++ user-service/src/main/java/rs/banka4/user_service/domain/card/db/AuthorizedUser.java
@@ -1,6 +1,8 @@
 package rs.banka4.user_service.domain.card.db;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import rs.banka4.user_service.domain.user.Gender;
 
 import java.time.LocalDate;
@@ -8,12 +10,13 @@ import java.util.UUID;
 
 @Embeddable
 public record AuthorizedUser(
-        UUID id,
+        UUID userId,
         String firstName,
         String lastName,
         LocalDate dateOfBirth,
         String email,
         String phoneNumber,
         String address,
+        @Enumerated(EnumType.STRING)
         Gender gender
 ) { }
