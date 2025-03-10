78aa6bcd8a885f0cdf228e3951cb6796d2c01889
Vasilije Jukic <vasilijejukic@gmail.com>
2025-03-10 14:15:13 +0100

    migration fix

--- user-service/src/main/resources/db/migration/V12__currency_alter.sql
+++ user-service/src/main/resources/db/migration/V12__currency_alter.sql
@@ -1,2 +1,6 @@
+DO $$
+BEGIN IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='currencies' AND column_name='version') THEN
 ALTER TABLE currencies
     ADD version BIGINT;
+END IF;
+END $$;
