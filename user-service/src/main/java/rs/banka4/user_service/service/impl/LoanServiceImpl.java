c23b18f04d675bf4297a99ed828fdd549ff01830
Ognjen Nikolic <onikolic12322rn@raf.rs>
2025-03-11 03:49:06 +0100

    added approveLoan and rejectLoan

--- user-service/src/main/java/rs/banka4/user_service/service/impl/LoanServiceImpl.java
+++ user-service/src/main/java/rs/banka4/user_service/service/impl/LoanServiceImpl.java
@@ -1,21 +1,22 @@
 package rs.banka4.user_service.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import rs.banka4.user_service.domain.loan.db.Loan;
 import rs.banka4.user_service.domain.loan.db.LoanStatus;
-import org.springframework.transaction.annotation.Transactional;
 import rs.banka4.user_service.domain.loan.dtos.LoanApplicationDto;
 import rs.banka4.user_service.domain.loan.dtos.LoanFilterDto;
 import rs.banka4.user_service.domain.loan.dtos.LoanInformationDto;
+import rs.banka4.user_service.exceptions.loan.LoanNotFound;
 import rs.banka4.user_service.domain.loan.mapper.LoanMapper;
 import rs.banka4.user_service.domain.loan.specification.LoanSpecification;
 import rs.banka4.user_service.repositories.LoanRepository;
-import rs.banka4.user_service.exceptions.loan.LoanNotFound;
 import rs.banka4.user_service.service.abstraction.LoanService;
 
 import java.util.Optional;
@@ -23,7 +24,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class LoanServiceImpl implements LoanService {
-
+  
     private final LoanRepository loanRepository;
     @Override
     public void createLoanApplication(LoanApplicationDto loanApplicationDto) {
