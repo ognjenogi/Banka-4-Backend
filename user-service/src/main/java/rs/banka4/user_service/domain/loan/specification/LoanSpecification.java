2ec9a935397196b758f7a09bd5fdf3b76ea5f9f3
Ognjen Nikolic <onikolic12322rn@raf.rs>
2025-03-14 04:27:48 +0100

    LoanSpecification for LoanRequest added

--- user-service/src/main/java/rs/banka4/user_service/domain/loan/specification/LoanSpecification.java
+++ user-service/src/main/java/rs/banka4/user_service/domain/loan/specification/LoanSpecification.java
@@ -7,6 +7,8 @@ import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
 import rs.banka4.user_service.domain.account.db.Account;
 import rs.banka4.user_service.domain.loan.db.Loan;
+import rs.banka4.user_service.domain.loan.db.LoanRequest;
+import rs.banka4.user_service.domain.loan.db.LoanStatus;
 import rs.banka4.user_service.domain.loan.dtos.LoanFilterDto;
 
 import java.util.ArrayList;
@@ -26,7 +28,14 @@ public class LoanSpecification {
             }
 
             if (filter.status() != null) {
-                predicates.add(builder.equal(root.get("status"), filter.status()));
+                if(filter.status().equals(LoanStatus.PROCESSING))
+                    predicates.add(builder.notEqual(root.get("status"), filter.status()));
+                else
+                    predicates.add(builder.equal(root.get("status"), filter.status()));
+            }
+
+            if (filter.status() == null) {
+                predicates.add(builder.notEqual(root.get("status"), LoanStatus.PROCESSING));
             }
 
             if (filter.accountNumber() != null && !filter.accountNumber().isEmpty()) {
@@ -37,7 +46,22 @@ public class LoanSpecification {
             return builder.and(predicates.toArray(new Predicate[0]));
         };
     }
+    public static Specification<LoanRequest> searchLoanRequests(LoanFilterDto filter) {
+        return (root, query, builder) -> {
+            List<Predicate> predicates = new ArrayList<>();
 
+            if (filter.type() != null) {
+                predicates.add(builder.equal(root.get("type"), filter.type()));
+            }
+
+            if (filter.accountNumber() != null && !filter.accountNumber().isEmpty()) {
+                Join<Loan, Account> accountJoin = root.join("account");
+                predicates.add(builder.equal(accountJoin.get("accountNumber"), filter.accountNumber()));
+            }
+
+            return builder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
     public static Specification<Loan> hasAccountNumber(String accountNumber) {
         return (root, query, criteriaBuilder) -> {
             Join<Loan, Account> accountJoin = root.join("account", JoinType.INNER);
