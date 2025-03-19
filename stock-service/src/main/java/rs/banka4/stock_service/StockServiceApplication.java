ee82a6864d94927a415fe1636c5b9c32d43f3029
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-03-20 00:38:14 +0100

    stock: new mICroSeRvICe

---
+++ stock-service/src/main/java/rs/banka4/stock_service/StockServiceApplication.java
@@ -0,0 +1,23 @@
+package rs.banka4.stock_service;
+
+import io.swagger.v3.oas.annotations.Operation;
+import java.util.Map;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StockServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockServiceApplication.class, args);
+    }
+
+    @GetMapping("/")
+    @Operation(summary = "Get a cute message")
+    public Map<String, ?> hello() {
+        return Map.of("hello", "world");
+    }
+}
