3135d1fe1270d8797b877e7b362cb4aaea8cd60b
iatanasijevic621rn <115571795+iatanasijevic621rn@users.noreply.github.com>
2025-03-19 23:48:15 +0100

    integration/ApproveLoanTest add approve loan integration test

---
+++ user-service/src/test/java/rs/banka4/user_service/integration/ApproveLoanTest.java
@@ -0,0 +1,71 @@
+package rs.banka4.user_service.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import rs.banka4.testlib.integration.DbEnabledTest;
+import rs.banka4.user_service.domain.account.db.Account;
+import rs.banka4.user_service.domain.loan.db.Loan;
+import rs.banka4.user_service.domain.loan.db.LoanStatus;
+import rs.banka4.user_service.integration.generator.UserGenerator;
+import rs.banka4.user_service.integration.seeder.TestDataSeeder;
+import rs.banka4.user_service.repositories.LoanRepository;
+import rs.banka4.user_service.utils.JwtUtil;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DbEnabledTest
+public class ApproveLoanTest {
+
+    @Autowired
+    private MockMvcTester m;
+
+    @Autowired
+    private UserGenerator userGen;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private ObjectMapper objMapper;
+
+    @Autowired
+    private LoanRepository loanRepository;
+
+    @Autowired
+    private TestDataSeeder testDataSeeder;
+
+    private Loan loan;
+    private String accessToken;
+
+    @BeforeEach
+    void setUp() {
+        Account account = testDataSeeder.seedAccount();
+        loan = testDataSeeder.seedLoan(account);
+
+        userGen.createEmployee(x -> x);
+        var toks = userGen.doEmployeeLogin("john.doe@example.com", "test");
+        accessToken = toks.accessToken();
+    }
+
+    @Test
+    void approveLoanSuccessfully() throws Exception {
+        m.put()
+                .uri("/loans/approve/" + loan.getLoanNumber())
+                .header("Authorization", "Bearer " + accessToken)
+                .accept(MediaType.APPLICATION_JSON)
+                .assertThat()
+                .hasStatus(HttpStatus.OK);
+
+        Loan approvedLoan = loanRepository.findByLoanNumber(loan.getLoanNumber()).orElseThrow();
+        assertThat(approvedLoan.getStatus()).isEqualTo(LoanStatus.APPROVED);
+    }
+}
