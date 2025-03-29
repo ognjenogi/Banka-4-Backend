d22621087020ae56a5897b050c00419cb497f9e6
DimitrijeA <53818425+SavkoDinamitas@users.noreply.github.com>
2025-03-29 22:41:57 +0100

    stock: return integration test

--- stock-service/src/test/java/rs/banka4/stock_service/StockServiceApplicationTests.java
+++ stock-service/src/test/java/rs/banka4/stock_service/StockServiceApplicationTests.java
@@ -1,15 +1,15 @@
-//package rs.banka4.stock_service;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import rs.banka4.testlib.integration.DbEnabledTest;
-//
-//@SpringBootTest
-//@DbEnabledTest
-//class StockServiceApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package rs.banka4.stock_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import rs.banka4.testlib.integration.DbEnabledTest;
+
+@SpringBootTest
+@DbEnabledTest
+class StockServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
