30abe6b8606d0f0bea9056291d5d2b3471268e50
Ognjen Nikolic <ognjennikolic203@gmail.com>
2025-04-01 22:55:20 +0200

    stock-service: SecurityType Enum for InfoDto added

--- stock-service/src/main/java/rs/banka4/stock_service/domain/listing/dtos/SecurityType.java
+++ stock-service/src/main/java/rs/banka4/stock_service/domain/listing/dtos/SecurityType.java
@@ -1,7 +1,22 @@
 package rs.banka4.stock_service.domain.listing.dtos;
 
+import lombok.Getter;
+import rs.banka4.stock_service.domain.options.db.Asset;
+import rs.banka4.stock_service.domain.options.db.Option;
+import rs.banka4.stock_service.domain.security.forex.db.ForexPair;
+import rs.banka4.stock_service.domain.security.future.db.Future;
+import rs.banka4.stock_service.domain.security.stock.db.Stock;
+
+@Getter
 public enum SecurityType {
-    FOREX_PAIR,
-    STOCK,
-    FUTURE
+    FUTURE(Future.class),
+    STOCK(Stock.class),
+    FOREX_PAIR(ForexPair.class),
+    OPTION(Option.class);
+
+    private final Class<? extends Asset> typeClass;
+
+    SecurityType(Class<? extends Asset> typeClass) {
+        this.typeClass = typeClass;
+    }
 }
