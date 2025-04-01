86b9938e66f22488e7d80d3ee4d1625537232012
LazarVuksanovic <lazar.vuksanovic17@gmail.com>
2025-04-01 14:55:23 +0200

    enable scheduling

--- stock-service/src/main/java/rs/banka4/stock_service/StockServiceApplication.java
+++ stock-service/src/main/java/rs/banka4/stock_service/StockServiceApplication.java
@@ -4,11 +4,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableScheduling
 public class StockServiceApplication {
 
     public static void main(String[] args) {
