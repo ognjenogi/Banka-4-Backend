4294ec81c65a64194ebb3a2856e2e0388252451f
LazarVuksanovic <lazar.vuksanovic17@gmail.com>
2025-04-01 13:16:22 +0200

    user-service/domain: add ActuaryPayloadDto

---
+++ user-service/src/main/java/rs/banka4/user_service/domain/user/employee/dtos/ActuatorPayloadDto.java
@@ -0,0 +1,41 @@
+package rs.banka4.user_service.domain.user.employee.dtos;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import rs.banka4.user_service.domain.currency.db.Currency;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Schema(description = "Payload for actuary-specific information")
+public record ActuatorPayloadDto(
+
+    @Schema(
+        description = "Indicates whether the actuary needs approval for operations",
+        example = "true"
+    )
+    @NotNull(message = "needsApproval is required")
+    boolean needsApproval,
+
+    @Schema(
+        description = "Limit amount assigned to the actuary",
+        example = "10000"
+    )
+    BigDecimal limitAmount,
+
+    @Schema(
+        description = "Currency code for the limit",
+        example = "RSD"
+    )
+    @NotNull(message = "Limit currency code is required")
+    Currency.Code limitCurrencyCode,
+
+    @Schema(
+        description = "ID of the actuary (user)",
+        example = "6d03b02b-b2d7-4de6-b2d5-9917f44d2f5a"
+    )
+    @NotNull(message = "Actuary ID is required")
+    UUID actuaryId
+
+) {
+}
