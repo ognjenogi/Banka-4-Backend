a2245f41eab7e184d51a74c8b25fe6b5f401244e
LazarVuksanovic <lazar.vuksanovic17@gmail.com>
2025-04-01 14:48:51 +0200

    add new privileges

--- common/src/main/java/rs/banka4/rafeisen/common/security/Privilege.java
+++ common/src/main/java/rs/banka4/rafeisen/common/security/Privilege.java
@@ -2,7 +2,10 @@ package rs.banka4.rafeisen.common.security;
 
 public enum Privilege {
     /* *NEVER* reorder elements in this enum - their order is significant. */
-    ADMIN;
+    ADMIN,
+    SUPERVISOR,
+    AGENT,
+    TRADE;
 
     public long bit() {
         return 1L << this.ordinal();
