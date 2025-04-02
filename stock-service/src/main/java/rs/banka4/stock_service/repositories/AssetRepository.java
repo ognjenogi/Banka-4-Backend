818a22575984fc6593d332f52a8629677eb54eff
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-04-02 04:04:48 +0200

    stock: add AssetRepository

---
+++ stock-service/src/main/java/rs/banka4/stock_service/repositories/AssetRepository.java
@@ -0,0 +1,12 @@
+package rs.banka4.stock_service.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import rs.banka4.stock_service.domain.options.db.Asset;
+
+@Repository
+public interface AssetRepository extends JpaRepository<Asset, UUID> {
+}
