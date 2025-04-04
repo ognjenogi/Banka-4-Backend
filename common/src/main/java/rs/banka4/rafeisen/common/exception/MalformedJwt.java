1bb489518ef8dd0ced53d3eeede7375e125ec512
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-04-04 13:23:49 +0200

    common: remove dead code

--- common/src/main/java/rs/banka4/rafeisen/common/exception/MalformedJwt.java
+++
@@ -1,7 +0,0 @@
-package rs.banka4.rafeisen.common.exception;
-
-public class MalformedJwt extends BaseApiException {
-    public MalformedJwt() {
-        super(HttpStatus.UNAUTHORIZED.getCode(), null);
-    }
-}
