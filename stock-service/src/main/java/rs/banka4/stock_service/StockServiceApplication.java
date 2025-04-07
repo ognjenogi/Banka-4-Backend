a7680d6dfa29b42d83ae685edd53ebbfc7159e82
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-04-07 11:20:40 +0200

    stock: remove hello world route

--- stock-service/src/main/java/rs/banka4/stock_service/StockServiceApplication.java
+++ stock-service/src/main/java/rs/banka4/stock_service/StockServiceApplication.java
@@ -1,26 +1,16 @@
 package rs.banka4.stock_service;
 
-import io.swagger.v3.oas.annotations.Operation;
-import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class StockServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StockServiceApplication.class, args);
-    }
-
-    @GetMapping("/")
-    @Operation(summary = "Get a cute message")
-    public Map<String, ?> hello() {
-        return Map.of("hello", "world");
     }
 }
