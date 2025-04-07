b08f7f1171835b93be10192ae99ee431c60fb8d4
LazarVuksanovic <lazar.vuksanovic17@gmail.com>
2025-04-08 00:04:30 +0200

    remove mapper from RetrofitConfig

--- stock-service/src/main/java/rs/banka4/stock_service/config/RetrofitConfig.java
+++ stock-service/src/main/java/rs/banka4/stock_service/config/RetrofitConfig.java
@@ -1,22 +1,15 @@
 package rs.banka4.stock_service.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
 
 @Configuration
 public class RetrofitConfig {
 
-    ObjectMapper mapper = new ObjectMapper();
-
-
     @Bean
     public Retrofit userServiceRetrofit() {
-        mapper.registerModule(new JavaTimeModule());
         return createRetrofit("http://gateway:80/");
     }
 
@@ -25,7 +18,6 @@ public class RetrofitConfig {
 
         return new Retrofit.Builder().baseUrl(baseUrl)
             .client(client)
-            .addConverterFactory(JacksonConverterFactory.create(mapper))
             .build();
     }
 }
