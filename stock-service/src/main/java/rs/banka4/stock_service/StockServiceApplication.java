4d180246d08ec7e83b5a60d9ece816163124d772
LazarVuksanovic <lazar.vuksanovic17@gmail.com>
2025-04-07 14:57:42 +0200

    format code

--- stock-service/src/main/java/rs/banka4/stock_service/StockServiceApplication.java
+++ stock-service/src/main/java/rs/banka4/stock_service/StockServiceApplication.java
@@ -1,13 +1,10 @@
 package rs.banka4.stock_service;
 
-import io.swagger.v3.oas.annotations.Operation;
-import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
