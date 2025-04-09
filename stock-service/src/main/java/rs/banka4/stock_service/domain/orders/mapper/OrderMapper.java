cc00a5b39ed1c78a489da087fc45234fdce60932
Vasilije Jukic <vasilijejukic@gmail.com>
2025-04-09 22:34:47 +0200

    stock-service: mapper update (toDto method)
    Co-authored-by: NAME <m.ljubic9802@gmail.com>
    Co-authored-by: ANOTHER-NAME <ooggii05@gmail.com>

--- stock-service/src/main/java/rs/banka4/stock_service/domain/orders/mapper/OrderMapper.java
+++ stock-service/src/main/java/rs/banka4/stock_service/domain/orders/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package rs.banka4.stock_service.domain.orders.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import rs.banka4.stock_service.domain.orders.db.Order;
 import rs.banka4.stock_service.domain.orders.dtos.CreateOrderDto;
