08faa947e377b65235120336b431cd5c57f0169e
Ognjen Nikolic <ognjennikolic203@gmail.com>
2025-04-11 01:16:50 +0200

    stock-service: implements both getMyRequests endpoints

--- stock-service/src/main/java/rs/banka4/stock_service/controller/OtcController.java
+++ stock-service/src/main/java/rs/banka4/stock_service/controller/OtcController.java
@@ -2,26 +2,70 @@ package rs.banka4.stock_service.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import retrofit2.Retrofit;
+import rs.banka4.rafeisen.common.security.AuthenticatedBankUserAuthentication;
+import rs.banka4.stock_service.config.clients.UserServiceClient;
 import rs.banka4.stock_service.controller.docs.OtcApiDocumentation;
 import rs.banka4.stock_service.domain.listing.dtos.ListingInfoDto;
+import rs.banka4.stock_service.domain.trading.db.OtcMapper;
+import rs.banka4.stock_service.domain.trading.db.dtos.OtcRequestDto;
+import rs.banka4.stock_service.service.abstraction.OtcRequestService;
 
 @RestController
 @RequestMapping("/otc")
 @RequiredArgsConstructor
 public class OtcController implements OtcApiDocumentation {
+    private final OtcRequestService otcRequestService;
+    private final Retrofit userServiceRetrofit;
+
     @Override
     @GetMapping("/me")
-    public Page<ListingInfoDto> getMyRequests(int page, int size, Authentication auth) {
-        return null;
+    public ResponseEntity<Page<OtcRequestDto>> getMyRequests(int page, int size, Authentication auth) {
+        return getRequestHelper(page, size, auth, false);
     }
 
     @Override
     @GetMapping("/me/unread")
-    public Page<ListingInfoDto> getMyRequestsUnread(int page, int size, Authentication auth) {
-        return null;
+    public ResponseEntity<Page<OtcRequestDto>> getMyRequestsUnread(int page, int size, Authentication auth) {
+        return getRequestHelper(page, size, auth, true);
     }
+
+    private ResponseEntity<Page<OtcRequestDto>> getRequestHelper(int page, int size, Authentication auth, boolean unread) {
+        UserServiceClient userServiceClient = userServiceRetrofit.create(UserServiceClient.class);
+        final var ourAuth = (AuthenticatedBankUserAuthentication) auth;
+        var myId = ourAuth.getPrincipal().userId();
+        var requests = unread ? otcRequestService.getMyRequestsUnread(PageRequest.of(page, size), myId) : otcRequestService.getMyRequests(PageRequest.of(page, size), myId);
+
+        Page<OtcRequestDto> dtoPage = requests.map(it -> {
+            try {
+                var resFor = userServiceClient.getUserInfo(it.getMadeFor()).execute();
+                var resBy = userServiceClient.getUserInfo(it.getMadeBy()).execute();
+                var resMod = userServiceClient.getUserInfo(it.getModifiedBy()).execute();
+
+                if (!resFor.isSuccessful() || resFor.body() == null ||
+                    !resBy.isSuccessful() || resBy.body() == null ||
+                    !resMod.isSuccessful() || resMod.body() == null) {
+                    throw new RuntimeException("User info service error");
+                }
+
+                String madeByStr = resBy.body().email();
+                String madeForStr = resFor.body().email();
+                String modifiedByStr = resMod.body().email();
+
+                return OtcMapper.INSTANCE.toOtcRequestDto(it, madeByStr, madeForStr, modifiedByStr);
+            } catch (Exception e) {
+                throw new RuntimeException("Error mapping OTC request", e);
+            }
+        });
+
+        return ResponseEntity.ok(dtoPage);
+    }
+
+
 }
