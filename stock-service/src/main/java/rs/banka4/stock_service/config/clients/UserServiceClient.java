e481d2bc7a6e05b9b05a47289251ef69a61ef1c5
DimitrijeA <53818425+SavkoDinamitas@users.noreply.github.com>
2025-04-11 01:29:41 +0200

    stock-service: fix retrofit for user fetch

--- stock-service/src/main/java/rs/banka4/stock_service/config/clients/UserServiceClient.java
+++ stock-service/src/main/java/rs/banka4/stock_service/config/clients/UserServiceClient.java
@@ -3,9 +3,13 @@ package rs.banka4.stock_service.config.clients;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 import rs.banka4.rafeisen.common.dto.EmployeeResponseDto;
+import rs.banka4.rafeisen.common.dto.UserResponseDto;
 import rs.banka4.stock_service.domain.response.PaginatedResponse;
+
+import java.util.UUID;
 
 public interface UserServiceClient {
 
@@ -19,4 +23,7 @@ public interface UserServiceClient {
         @Query("page") int page,
         @Query("size") int size
     );
+
+    @GET("user/{id}")
+    Call<UserResponseDto> getUserInfo(@Path("id") UUID id, @Header("Authorization") String token);
 }
