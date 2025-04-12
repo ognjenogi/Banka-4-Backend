c92a77e9576b57683c947b2567378cb78aa7d8b7
Ognjen Nikolic <ognjennikolic203@gmail.com>
2025-04-13 01:14:03 +0200

    stock-service:adds cron job for expired otc requests

--- stock-service/src/main/java/rs/banka4/stock_service/repositories/OtcRequestRepository.java
+++ stock-service/src/main/java/rs/banka4/stock_service/repositories/OtcRequestRepository.java
@@ -1,5 +1,7 @@
 package rs.banka4.stock_service.repositories;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import rs.banka4.stock_service.domain.trading.db.OtcRequest;
+import rs.banka4.stock_service.domain.trading.db.RequestStatus;
 
 public interface OtcRequestRepository extends JpaRepository<OtcRequest, UUID> {
 
@@ -27,4 +30,5 @@ public interface OtcRequestRepository extends JpaRepository<OtcRequest, UUID> {
         @Param("userId") String userId,
         Pageable pageable
     );
+    List<OtcRequest> findAllByStatusAndSettlementDateBefore(RequestStatus status, LocalDate date);
 }
