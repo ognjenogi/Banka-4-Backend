57afb0d06a860a66b5ff9236074737b4287c092e
Ognjen Nikolic <ognjennikolic203@gmail.com>
2025-04-16 21:33:32 +0200

    stock-service:add TaxableUserDto

---
+++ bank-service/src/main/java/rs/banka4/bank_service/domain/taxes/db/dto/TaxableUserDto.java
@@ -0,0 +1,26 @@
+package rs.banka4.bank_service.domain.taxes.db.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import rs.banka4.rafeisen.common.currency.CurrencyCode;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Schema(description = "Summary entry for a user eligible for capital gains tax")
+public record TaxableUserDto(
+    @Schema(description = "User ID", example = "f47ac10b-58cc-4372-a567-0e02b2c3d479")
+    UUID userId,
+
+    @Schema(description = "First name", example = "John")
+    String firstName,
+    @Schema(description = "email", example = "John@gmail.com")
+    String email,
+
+    @Schema(description = "Last name", example = "Doe")
+    String lastName,
+
+    @Schema(description = "Current unpaid tax amount in RSD", example = "1200.50")
+    BigDecimal unpaidTax,
+    @Schema(description = "Currency of tax", example = "RSD")
+    CurrencyCode currency
+) {}
