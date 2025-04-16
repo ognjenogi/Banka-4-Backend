f9ff63d574fe8c5b11ac5261f5b31cd3b0172418
DimitrijeA <53818425+SavkoDinamitas@users.noreply.github.com>
2025-04-16 15:09:51 +0200

    bank-service: add OptionsApiDocumentation

--- bank-service/src/main/java/rs/banka4/bank_service/domain/options/dtos/BuyOptionRequestDto.java
+++ bank-service/src/main/java/rs/banka4/bank_service/domain/options/dtos/BuyOptionRequestDto.java
@@ -1,10 +1,12 @@
 package rs.banka4.bank_service.domain.options.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.UUID;
 
+@Schema(description = "request dto to buy an option")
 public record BuyOptionRequestDto(
-    UUID optionId,
-    String accountNumber,
-    int amount
+    @Schema(description = "option to buy id") UUID optionId,
+    @Schema(description = "user's account to take premium amount from") String accountNumber,
+    @Schema(description = "amount of options i want to buy for this stock") int amount
 ) {
 }
--- bank-service/src/main/java/rs/banka4/bank_service/domain/options/dtos/UseOptionRequest.java
+++ bank-service/src/main/java/rs/banka4/bank_service/domain/options/dtos/UseOptionRequest.java
@@ -1,9 +1,13 @@
 package rs.banka4.bank_service.domain.options.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.UUID;
 
+@Schema(description = "request dto for use option route")
 public record UseOptionRequest(
-    UUID optionId,
-    String accountNumber
+    @Schema(description = "option to use id") UUID optionId,
+    @Schema(
+        description = "user's account to take money from or give money to (depends on option type)"
+    ) String accountNumber
 ) {
 }
