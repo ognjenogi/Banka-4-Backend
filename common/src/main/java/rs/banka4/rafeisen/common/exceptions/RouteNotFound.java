376607b647746d1f510be9c1d1a81512a68a83e1
Arsen Arsenović <aarsenovic8422rn@raf.rs>
2025-04-18 02:45:27 +0200

    common,*: provide a path in routenotfound

--- common/src/main/java/rs/banka4/rafeisen/common/exceptions/RouteNotFound.java
+++ common/src/main/java/rs/banka4/rafeisen/common/exceptions/RouteNotFound.java
@@ -1,9 +1,10 @@
 package rs.banka4.rafeisen.common.exceptions;
 
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 
 public class RouteNotFound extends BaseApiException {
-    public RouteNotFound() {
-        super(HttpStatus.NOT_FOUND, null);
+    public RouteNotFound(String path) {
+        super(HttpStatus.NOT_FOUND, Map.of("path", path));
     }
 }
